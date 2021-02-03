f9db883922c1288596de10690e790bea487290ed
Maurice Tadros <maurice@bowriverstudio.com>
2021-02-03 11:38:23 -0700

    Updated Eslinter docs

--- types.ts
+++ types.ts
@@ -2,9 +2,21 @@ import Data from "@frontity/source/types";
 import WpSource from "@frontity/wp-source/types";
 import { MergePackages, Package, ServerAction } from "frontity/types";
 
+/**
+ * Helper package to assist fetching the menu from the WP API.
+ */
 export interface FrontityMenu extends Package {
+  /**
+   *  Name of the package.
+   */
   name: "frontity-menu";
+  /**
+   * The actions exposed by this package.
+   */
   actions: {
+    /**
+     * Frontitymenu namespace.
+     */
     frontitymenu: {
       /**
        * Before SSR action, to fetch the menu content from the REST API.
@@ -15,15 +27,18 @@ export interface FrontityMenu extends Package {
 }
 
 /**
- * Data for a Menu
+ * Data for a Menu.
  */
 export interface MenuData extends Data {
   /**
-   * Menu Items
+   * Menu Items.
    */
   items: [NavigationMenuItem];
 }
 
+/**
+ * TODO a subset of the data returned from the API.
+ */
 type NavigationMenuItem = {
   /**
    * Title of the menu item.
